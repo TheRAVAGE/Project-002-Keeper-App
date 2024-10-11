f229d30db869cc2f62946910d9c5abbc257be3be
Akhil M Mathew <iamakhil99@gmail.com>
2024-10-12 01:21:31 +0530

    deploying 000

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {year}, Akhil M Mathew</p>
     </footer>
   );
 }
